e72653f2af9734c5c2673a16f163433afa264862
Tyrone Michael Avnit <tyronemichael@gmail.com>
2012-12-05 06:29:20 +0200

    Updated API Url so only enabled items are pulled through.

--- js/freshive.fnbFbNav.js
+++ js/freshive.fnbFbNav.js
@@ -18,7 +18,7 @@
   var pluginName = 'FNBFbMenu',
       defaults = {
         //dataUrl: "../nav/sample.json"
-        dataUrl: "//fnbsocialmedia.co.za/hubbeta/api/fbmenu?callback=?"
+        dataUrl: "//fnbsocialmedia.co.za/fnbhub/api/fbmenu?callback=?"
       };
 
   // The actual plugin constructor
